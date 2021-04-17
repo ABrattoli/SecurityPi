71d6aaf83dde1491028b37544d4478f7c062f4ba
ABrattoli <45080421+ABrattoli@users.noreply.github.com>
2021-04-16 21:29:16 -0400

    Update exect.sh

--- exect.sh
+++ exect.sh
@@ -1,13 +1,9 @@
 sudo apt-get install curl
-
 curl -sSL https://install.pi-hole.net | bash
-
 wget https://github.com/mattymcfatty/HoneyPi/archive/master.zip
 unzip master.zip
 cd HoneyPi-master
 chmod +x *.sh
 sudo ./honeyPiInstaller.sh
-
 sudo apt-get install arpwatch
-
 Sudo apt-get install cacti
